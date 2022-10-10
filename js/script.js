cbd124c4a06ca147d2a93469a9c07c4213e8cb26
sder12 <sara.cetto@gmail.com>
2022-10-10 15:06:02 +0200

    Instruction Js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 // DEBUG 
 console.log('Hello');
+
+ //Scrivi un programma che stampi in console i numeri da 1 a 100
+ //multipli 3 stampi “Fizz” - 5 stampi “Buzz” -multipli di 3 che di 5 stampi “FizzBuzz” --> %
+
+ //CREARE INTERAZIONE i tra 1 e 100
+ //creare una LET contentDiv ="";
+ //creare all'interno del loop una CONDIZIONE if %2===0 divisibile per 2 etc. contentDiv = fizz
+    // dare al divisibile per pari il nome fizz- per 5 Buzz per entrambi FIZZBUZZ --> else con numero=i?
+//con createElement creare all'interno della row > <div class="col"> ${contentDiv} </div>   -->
+//creare una seconda CONDIZIONE in base al ${contentDiv} adding class: bg-numb bg-fzz bg-bzz bg-fzzbzz
